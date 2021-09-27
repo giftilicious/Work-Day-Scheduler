9ae9174a74ea06e7ec17ac5631fec7d5a87f2f6b
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-09-27 19:33:51 -0400

    Added elements for remaining time blocks in HTML Created functions for colour-coding and user input in the js file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Work-Day-Scheduler
+This an interactive day planner that allows the user to enter tasks for his/her work day. Each task that is entered is saved for the user's reference. Also, each hour is colour-coded to indicate whether the block of time is in the past, present or future. 
+
+https://giftilicious.github.io/Work-Day-Scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,93 @@
-let taskEl = document.querySelector('.form-control');
-let userInput = localStorage.getItem('inlineFormInputGroupUsername2');
+let timeblockEl = document.querySelector('#currentHour');
+let taskEl = document.querySelector('.form-control').value;
+let saveBtn = document.querySelector('.btn');
 
-// Shows today's date
+// let userInput = localStorage.setItem('inlineFormInputGroupUsername2');
+
+// Time Section
 let today = moment();
+//let hourFormatted = today.format("HH")
 $("#currentDay").text(today.format('MMM Do, YYYY'));
-
+let currentTime = today.hours()
 // Current time
-let currentTime = moment().format('LTS');
-$("#currentHour").add('hide') 
+// let currentTime = moment().format('LTS');
+// $("#currentHour").text(moment(today).format('hh:mm:ss'));
+// // Past time
+// let pastTime = moment().startOf('hour').fromNow();
+// // moment().subtract(1, 'hour');
+// // Future time
+// let futureTime = moment().add(1, 'hour');
+// console.log(currentTime, pastTime, futureTime);
+console.log(currentTime)
 
-// Past time
-let pastTime = moment().startOf('hour').fromNow();
+
+// Colour Coding
+$('.card-header').each(function() {
+    let cardTime = $(this).data('time')
+    console.log(cardTime)
+    if (currentTime > cardTime) {
+        $(this).parent().siblings('.input-group').children('.form-control').addClass('past')
+    } else if (currentTime === cardTime) {
+        console.log('present')
+        $(this).parent().siblings('.input-group').children('.form-control').addClass('present')
+    } else {
+        $(this).parent().siblings('.input-group').children('.form-control').addClass('future')
+    }
+});
+
+// Store user's input
+$('.btn').on('click', function() {
+    let text = $(this).parent().siblings('.input-group').children('.form-control').val()
+    console.log(text)
+});
 
 
-// moment().subtract(1, 'hour');
+// Put input into local storage
 
-// Future time
-let futureTime = moment().add(1, 'hour');
 
+///////////////////////////////////////////////////
+
+
+
+// Save user input on screen
+// localStorage.getItem('inlineFormInputGroupUsername2');
+// localStorage.setItem('inlineFormInputGroupUsername2');
+function renderTasks() {
+    let taskInput = localStorage.getItem('inlineFormInputGroupUsername2')
+        
+}
+
+taskButton.addEventListener("click", function(event)
+ {
+    event.preventDefault();
+    localStorage.setItem(taskInput)
+ });
 
 // To set the input section based on colour
 // Past: taskEl.classList.add('past)
 // Present: taskEl.classList.add('present)
 // Future: taskEl.classList.add('future)
-function setTimeStatus() {
-    // let plannerTime = document.querySelector('#currentHour');
+// function setTimeStatus() {
+//     // let plannerTime = document.querySelector('#currentHour');
     
-    if (currentTime) {
-        console.log('present');
-        taskEl.classList.add('.present')
-    } else if (pastTime) {
-        console.log('past');
-        taskEl.classList.add('.past')
-    } else {
-        console.log('future');
-        taskEl.classList.add('.future')
-    }
-}
-setTimeStatus();
+// function timeblockColor() {
+//     if (currentTime) {
+//         console.log('present');
+//         taskEl.classList.add('.present')
+//     } else if (pastTime) {
+//         console.log('past');
+//         taskEl.classList.add('.past')
+//     } else {
+//         console.log('future');
+//         taskEl.classList.add('.future')
+//     }
+// } 
 
-// Save user input on screen
-// localStorage.getItem('inlineFormInputGroupUsername2');
-// localStorage.setItem('inlineFormInputGroupUsername2');
+// setTimeStatus();
+
+//if currentHour < dataAttrValue)
+// {
+//     then apply my css classes
+// }
+
+// use forEach to loop through 'data-time' id
